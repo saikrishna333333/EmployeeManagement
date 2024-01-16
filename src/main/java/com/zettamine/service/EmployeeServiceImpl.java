697ee7fa923beeca13ad191c0d8bb76fc4197d4c
Saikrishna <saikrishna3889@gmail.com>
2024-01-16 15:24:57 +0530

    added service class implementation methods

---
+++ src/main/java/com/zettamine/service/EmployeeServiceImpl.java
@@ -0,0 +1,31 @@
+package com.zettamine.service;
+
+import com.zettamine.DAO.Employee;
+
+public class EmployeeServiceImpl implements EmployeeServices {
+
+	@Override
+	public void saveEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getEmpByid(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
